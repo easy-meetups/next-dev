ea8dea41c249804d8a09824ee0decab9429ef170
Dario Reis <85812823+darioreisjr@users.noreply.github.com>
2025-06-10 08:39:28 -0300

    Add PWA setup

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,21 @@
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+});
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: ['ui-avatars.com','oclb.com.br','blog.zooxsmart.com','encrypted-tbn0.gstatic.com','miro.medium.com'],
+    domains: [
+      'ui-avatars.com',
+      'oclb.com.br',
+      'blog.zooxsmart.com',
+      'encrypted-tbn0.gstatic.com',
+      'miro.medium.com',
+    ],
   },
-}
+};
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
